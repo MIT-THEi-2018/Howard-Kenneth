786fb135154239bf75850457fe34cf3253fea77c
kenneth123321 <41935317+kenneth123321@users.noreply.github.com>
2018-08-03 02:03:08 +0800

    Add files via upload

---
+++ UI.py
@@ -0,0 +1,43 @@
+from tkinter import *
+#pip install pillow
+from PIL import Image, ImageTk
+
+class UI:
+	
+	def __init__(self,master):
+		frame = Frame(master, bg="#aedb9f")
+		frame.pack()
+
+		self.label1 = Label(frame, text = "---------This App can recommend other Animes that you may like---------", fg = "red", font='Helvetica 13 bold', bg="#aedb9f")
+		self.label1.pack()
+
+		self.label2 = Label(frame, text = "Enter The ID of An Anime You Watch ", font='Helvetica 10 bold', bg="#aedb9f")
+		self.label2.pack()
+
+		self.input = Entry(frame, font='Helvetica 8 bold')
+		self.input.pack()
+
+		self.space = Label(frame, bg="#aedb9f")
+		self.space.pack()
+
+		self.sumbit = Button(frame, text = "Submit", command = self.search)
+		self.sumbit.pack()
+		
+		self.space = Label(frame, bg="#aedb9f")
+		self.space.pack()
+
+		self.result = Text(frame, wrap = WORD, font='Helvetica 10 bold')
+		self.result.pack(fill = Y)
+
+	def search(self):
+		anime_id = self.input.get()
+		self.result.delete(0.0, END)
+		self.result.insert(0.0,anime_id)
+
+
+root = Tk()
+root.title("AnimeRecommendation")
+root.configure(background="#aedb9f")
+root.geometry("600x400")
+App = UI(root)
+root.mainloop()
